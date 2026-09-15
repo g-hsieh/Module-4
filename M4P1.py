60a290873529baac47c0514b3e99ca06218441fc
g-hsieh <g_hsieh@mail.harpercollege.edu>
2026-09-14 21:03:08 -0500

    Add files via upload

---
+++ M4P1.py
@@ -0,0 +1,10 @@
+print("Enter the Score of the First Exam ")
+firstExamScore = int(input())
+firstExamPoints = firstExamScore * 0.6
+print("The first exam total score is " + str(firstExamPoints))
+print("Enter the Score of the Second Exam ")
+secondExamScore = int(input())
+secondExamPoints = secondExamScore * 0.4
+print("The second exam total score is " + str(secondExamPoints))
+totalExamScore = firstExamScore + secondExamScore
+print("The total score for both exams is " + str(totalExamScore))
